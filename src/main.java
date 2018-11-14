79324e0e378fa36b8b2909056a438afc73eaa91d
A-Petersen <a.petersen@stud.fh-luebeck.de>
2018-11-14 18:55:34 +0100

    lul

--- src/main.java
+++ src/main.java
@@ -2,6 +2,11 @@ import org.jzy3d.analysis.AnalysisLauncher;
 
 public class main {
     public static void main(String[] args) throws Exception {
+        AnalysisLauncher.open(new Test(new View(9, 20, 'A')));
+        AnalysisLauncher.open(new Test(new View(10, 20, 'A')));
+        AnalysisLauncher.open(new Test(new View(11, 20, 'A')));
         AnalysisLauncher.open(new Test(new View(9, 20, 'B')));
+        AnalysisLauncher.open(new Test(new View(10, 20, 'B')));
+        AnalysisLauncher.open(new Test(new View(11, 20, 'B')));
     }
 }
